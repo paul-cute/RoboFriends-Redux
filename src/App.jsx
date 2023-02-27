c1b171dcc8cd25dd6b52a3e066329cdd2489660e
paul-cute <paulcuteflorin@protonmail.com>
2023-02-27 23:43:53 +0200

    Fecth data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 import CardList from './CardList'
 import { robots } from './robots' 
 import SearchBox from './SearchBox'
+import './App.css'
 export default class App extends Component {
 
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {return response.json()})
+            .then(users =>{this.setState({robots: users})})  
     }
 
     onSearchChange = (event) =>{
@@ -21,12 +28,17 @@ export default class App extends Component {
     const filterRobot = this.state.robots.filter(robot => {
         return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList robots ={filterRobot}/>
-      </div>
-    )
-  }
+
+    if(this.state.robots.length === 0 ){
+        return <h1>Loading</h1>
+    }else {
+        return (
+        <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange = {this.onSearchChange}/>
+            <CardList robots ={filterRobot}/>
+        </div>
+        )
+    }
+    }
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,6 @@ export default class Card extends Component {
     console.log(this.props.id)
     return (
       <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
-        <h1>Robots</h1>
         <img src={`https://robohash.org/${this.props.id}?200x200`} alt="robot" />
         <div>
             <h2>{this.props.name}</h2>
