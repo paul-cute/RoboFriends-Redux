493af2705045c6e680e729a80df9082e0807edb1
paul-cute <paulcuteflorin@protonmail.com>
2023-02-27 23:19:24 +0200

    Search Robot

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
 import { robots } from './robots' 
-
+import SearchBox from './SearchBox'
 export default class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({searchfield: event.target.value})
+    }
+
   render() {
+
+    const filterRobot = this.state.robots.filter(robot => {
+        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
     return (
-      <div>
+      <div className='tc'>
         <h1>RoboFriends</h1>
-        <CardList robots ={robots}/>
+        <SearchBox searchChange = {this.onSearchChange}/>
+        <CardList robots ={filterRobot}/>
       </div>
     )
   }
